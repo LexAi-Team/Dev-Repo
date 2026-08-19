55c52e0b206cc492c980f810099a94d303ce26dd
Kavin Raj S <skavinraj.dev@gmail.com>
2026-08-19 11:51:22 +0530

    Delete Code directory

--- Code/Frontend/frontend/src/services/aiService.js
+++
@@ -1,9 +0,0 @@
-import api from '../api/axios';
-
-export const askLexAI = async (query) => {
-  if (!query) throw new Error('Missing query');
-  const { data } = await api.post('/ask', { query });
-  return data;
-};
-
-export default { askLexAI };
